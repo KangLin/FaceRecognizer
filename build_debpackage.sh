19dded4ec66b922d33a82e529b73f1316171fa96
Kang Lin <kl222@126.com>
2023-08-11 11:52:29 +0800

    CI: Use qt6 in ubuntu.yml

--- build_debpackage.sh
+++ build_debpackage.sh
@@ -11,10 +11,6 @@ if [ -n "$1" -a -z "$QT_ROOT" ]; then
     QT_ROOT=$1
 fi
 
-if [ -z "$QT_ROOT" ]; then
-    help "QT_ROOT don't exist: $QT_ROOT"
-fi
-
 if [ -n "$2" ]; then
     export ThirdLibs_DIR=$2
 fi
@@ -44,9 +40,7 @@ if [ -z "$ENABLE_DOWNLOAD_MODULE" ]; then
 fi
 
 export RabbitCommon_DIR=$RabbitCommon_DIR
-export QT_ROOT=$QT_ROOT
-export PATH=$QT_ROOT/bin:$PATH
-export LD_LIBRARY_PATH=$QT_ROOT/lib/i386-linux-gnu:$QT_ROOT/lib:$LD_LIBRARY_PATH
-export PKG_CONFIG_PATH=$QT_ROOT/lib/pkgconfig:$PKG_CONFIG_PATH
-fakeroot debian/rules binary 
-#dpkg-buildpackage -us -uc -b
+#fakeroot debian/rules binary
+#dpkg-buildpackage -us -uc -b  #The -us -uc tell it there is no need to GPG sign the package. the -b is build binary
+dpkg-buildpackage -us -uc   #The -us -uc tell it there is no need to GPG sign the package
+
